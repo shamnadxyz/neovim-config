6b6d5cc35ca1956b579c9bb5d3467ecad38e5f86
shamnadxyz <shamnadxyz@proton.me>
2023-12-11 11:57:01 +0000

    Telescope builtin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,4 +7,4 @@ vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' }
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
-
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
