74d98f6bc6e2293f37c3e0ccaaa5335997168c73
shamnadxyz <shamnadxyz@proton.me>
2025-09-10 15:55:39 +0000

    fix: Fix python formatter name

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -16,12 +16,16 @@ return {
         formatters_by_ft = {
           lua = { "stylua" },
           -- Conform will run multiple formatters sequentially
-          python = { "ruff" },
+          python = { "ruff_format" },
           -- You can customize some of the format options for the filetype (:help conform.format)
           rust = { "rustfmt", lsp_format = "fallback" },
           -- Conform will run the first available formatter
           javascript = { "prettierd", "prettier", stop_after_first = true },
         },
+        default_format_opts = {
+          lsp_format = "fallback",
+        },
+        notify_on_error = true,
       })
     end,
   },
