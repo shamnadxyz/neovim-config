fe0c4dc7925a7d322bc53c9904b0403b61e8661d
shamnadxyz <shamnadxyz@proton.me>
2025-09-11 13:51:05 +0000

    chore(keymaps): remove arrow key restrictions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -53,10 +53,3 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 -- System Operations
 -- Disable Ex mode (accidental Q press)
 vim.keymap.set("n", "Q", "<nop>")
-
--- Navigation Safety
--- Disable arrow keys with helpful guidance
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
