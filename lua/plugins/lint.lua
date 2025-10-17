45be61c4cd3d9c590288cc2a02d77a9e6724d5a1
shamnadxyz <shamnadxyz@proton.me>
2025-10-17 15:30:24 +0000

    refactor: clean up comments and rename augroup

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,6 +1,6 @@
 return {
 
-  { -- Linting
+  {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
@@ -8,8 +8,6 @@ return {
       lint.linters_by_ft = {
         markdown = { "markdownlint" },
       }
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = lint_augroup,
