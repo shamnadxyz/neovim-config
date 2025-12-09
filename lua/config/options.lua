52df7b556e97d70f9fa79714b5541158acba689d
shamnadxyz <shamnadxyz@proton.me>
2025-12-09 13:08:35 +0000

    feat(options): add sidescrolloff setting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,6 +16,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Minimal number of screen columns to keep to the left and right of the cursor
+vim.opt.sidescrolloff = 10
+
 -- If performing an operation that would fail due to unsaved changes (like `:q`),
 -- raise a dialog asking if you wish to save the current file(s)
 vim.opt.confirm = true
