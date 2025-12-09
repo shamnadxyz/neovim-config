93ba6e69fc5e2406d092aae0b3c0257196af953c
shamnadxyz <shamnadxyz@proton.me>
2025-12-09 08:52:25 +0000

    feat(treesitter): enable incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,15 @@ return {
         enable = true,
       },
       indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
     },
   },
 }
