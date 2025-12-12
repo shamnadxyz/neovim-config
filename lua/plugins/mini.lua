ffec1e9c781a1b719f2bb7391b3dd7c69c06617a
shamnadxyz <shamnadxyz@proton.me>
2025-12-12 14:59:32 +0000

    feat: add mini.ai, mini.surround, and mini.operator

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,5 @@
+return {
+  { 'nvim-mini/mini.ai', version = '*', opts = {} },
+  { 'nvim-mini/mini.pairs', version = '*', opts = {} },
+  { 'nvim-mini/mini.operators', version = '*', opts = {} },
+}
