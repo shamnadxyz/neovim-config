b0a1104cad62edd6a6b0bd818a8423ac473d610d
shamnadxyz <shamnadxyz@proton.me>
2025-12-13 04:47:20 +0000

    Revert "feat(treesitter): enable incremental selection"
    This reverts commit 93ba6e69fc5e2406d092aae0b3c0257196af953c.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,15 +10,6 @@ return {
         enable = true,
       },
       indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-space>',
-          scope_incremental = false,
-          node_decremental = '<bs>',
-        },
-      },
     },
   },
 }
