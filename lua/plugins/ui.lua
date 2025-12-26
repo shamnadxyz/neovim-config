927134947511135b94dc6e4ad3211bbba098a877
shamnadxyz <shamnadxyz@proton.me>
2025-12-26 13:02:33 +0000

    feat: replace lualine with mini.statusline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -13,76 +13,7 @@ return {
     },
     lazy = false,
   },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-          statusline = {},
-          winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-          refresh_time = 16, -- ~60fps
-          events = {
-            'WinEnter',
-            'BufEnter',
-            'BufWritePost',
-            'SessionLoadPost',
-            'FileChangedShellPost',
-            'VimResized',
-            'Filetype',
-            'CursorMoved',
-            'CursorMovedI',
-            'ModeChanged',
-          },
-        },
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = {
-          {
-            'filename',
-            symbols = {
-              readonly = ' ',
-            },
-          },
-        },
-        lualine_x = {
-          {
-            'filetype',
-            icon_only = true,
-          },
-        },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      winbar = {},
-      inactive_winbar = {},
-      extensions = {},
-    },
-  },
+  { 'nvim-mini/mini.statusline', version = '*', opts = {} },
   {
     'folke/which-key.nvim',
     event = 'VeryLazy',
