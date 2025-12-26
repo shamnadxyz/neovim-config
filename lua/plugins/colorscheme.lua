3d09a0a4ded51cbefa021a0565e7239742afd835
shamnadxyz <shamnadxyz@proton.me>
2025-12-26 13:04:22 +0000

    feat: remove transparency toggle from catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,34 +1,17 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    local transparency = true
-    local catppuccin = require 'catppuccin'
-
-    catppuccin.setup {
-      transparent_background = transparency,
-      float = {
-        transparent = true,
-        solid = true,
-      },
-    }
-
-    -- Function to toggle transparency
-    local function toggle_transparency()
-      transparency = not transparency
-      catppuccin.setup {
-        transparent_background = transparency,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        transparent_background = false,
+        float = {
+          transparent = true,
+          solid = true,
+        },
       }
       vim.cmd.colorscheme 'catppuccin'
-    end
-
-    vim.keymap.set('n', '<leader>tt', toggle_transparency, {
-      noremap = true,
-      silent = true,
-      desc = 'Toggle transparency',
-    })
-
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+    end,
+  },
 }
