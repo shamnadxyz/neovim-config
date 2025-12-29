19269f6c47b6c45ae6d00e00263e23e9def6b591
shamnadxyz <shamnadxyz@proton.me>
2025-12-29 04:58:58 +0000

    feat(colorscheme): add transparency toggle

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,13 +4,32 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      require('catppuccin').setup {
-        transparent_background = false,
+      local catppuccin = require 'catppuccin'
+      local opts = {
+        auto_integrations = true,
+        transparent_background = true,
         float = {
           transparent = true,
           solid = true,
         },
       }
+
+      -- Initial setup
+      catppuccin.setup(opts)
+
+      -- Toggle transparency
+      local function toggle_transparency()
+        opts.transparent_background = not opts.transparent_background
+        catppuccin.setup(opts)
+        vim.cmd.colorscheme 'catppuccin'
+      end
+
+      vim.keymap.set('n', '<leader>tt', toggle_transparency, {
+        noremap = true,
+        silent = true,
+        desc = 'Toggle transparency',
+      })
+
       vim.cmd.colorscheme 'catppuccin'
     end,
   },
