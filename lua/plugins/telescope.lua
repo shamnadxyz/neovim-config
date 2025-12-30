b49e55352c6fcd65fc79df9274b8e2c9f00384b0
shamnadxyz <shamnadxyz@proton.me>
2025-12-30 06:33:50 +0000

    chore: update telescope and treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,11 @@
 return {
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      {
         'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install',
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
