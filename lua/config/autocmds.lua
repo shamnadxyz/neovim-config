3800f51a7314fe135a53a6ab299af89cee8e9457
shamnadxyz <shamnadxyz@proton.me>
2025-12-30 16:57:34 +0000

    feat(autocmds): resize splits if window resized

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -53,3 +53,13 @@ vim.api.nvim_create_autocmd('FileType', {
     end, { buffer = event.buf, silent = true })
   end,
 })
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+  group = vim.api.nvim_create_augroup('resize_splits', { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabnext ' .. current_tab)
+  end,
+})
