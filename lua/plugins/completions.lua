83b3fcd8776a05dd7171b0254f9b7f2ac50440ff
shamnadxyz <shamnadxyz@proton.me>
2026-01-05 06:07:37 +0000

    feat: remove blink.pairs plugin

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -91,33 +91,4 @@ return {
       'sources.default',
     },
   },
-  {
-    'saghen/blink.pairs',
-    version = '*',
-
-    -- download prebuilt binaries from github releases
-    dependencies = 'saghen/blink.download',
-
-    --- @module 'blink.pairs'
-    --- @type blink.pairs.Config
-    opts = {
-      mappings = {
-        -- you can call require("blink.pairs.mappings").enable()
-        -- and require("blink.pairs.mappings").disable()
-        -- to enable/disable mappings at runtime
-        enabled = true,
-        cmdline = true,
-        -- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
-        -- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
-        disabled_filetypes = {},
-        -- see the defaults:
-        -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
-        pairs = {},
-      },
-      highlights = {
-        enabled = false,
-      },
-      debug = false,
-    },
-  },
 }
