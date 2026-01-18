1bd3072c5e7659ee3f65c34566312a7d65573312
shamnadxyz <shamnadxyz@proton.me>
2026-01-18 18:19:35 +0000

    feat(plugins): replace oil with mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,4 +4,59 @@ return {
   { 'nvim-mini/mini.operators', version = '*', opts = {} },
   { 'nvim-mini/mini.comment', version = '*', opts = {} },
   { 'nvim-mini/mini.icons', version = '*', opts = {} },
+  {
+    'nvim-mini/mini.files',
+    version = '*',
+    config = function()
+      local minifiles = require 'mini.files'
+      minifiles.setup {
+
+        -- Module mappings created only inside explorer.
+        -- Use `''` (empty string) to not create one.
+        mappings = {
+          close = 'q',
+          go_in = 'l',
+          go_in_plus = 'L',
+          go_out = 'h',
+          go_out_plus = 'H',
+          mark_goto = "'",
+          mark_set = 'm',
+          reset = '<BS>',
+          reveal_cwd = '@',
+          show_help = 'g?',
+          synchronize = '=',
+          trim_left = '<',
+          trim_right = '>',
+        },
+
+        -- General options
+        options = {
+          -- Whether to delete permanently or move into module-specific trash
+          permanent_delete = false,
+          -- Whether to use for editing directories
+          use_as_default_explorer = true,
+        },
+
+        -- Customization of explorer windows
+        windows = {
+          -- Maximum number of windows to show side by side
+          max_number = math.huge,
+          -- Whether to show preview of file/directory under cursor
+          preview = true,
+          -- Width of focused window
+          width_focus = 50,
+          -- Width of non-focused window
+          width_nofocus = 50,
+          -- Width of preview window
+          width_preview = 50,
+        },
+      }
+
+      vim.keymap.set('n', '<leader>e', function()
+        if not minifiles.close() then
+          minifiles.open()
+        end
+      end, { desc = 'Open Oil file explorer' })
+    end,
+  },
 }
