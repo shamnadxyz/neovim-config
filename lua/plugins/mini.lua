2961986def6a2f1f3b8bb783e2d702c466df1ccd
shamnadxyz <shamnadxyz@proton.me>
2026-01-28 15:31:45 +0000

    feat: remove mini.comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,7 +2,6 @@ return {
   { 'nvim-mini/mini.ai', version = '*', opts = {} },
   { 'nvim-mini/mini.surround', version = '*', opts = {} },
   { 'nvim-mini/mini.operators', version = '*', opts = {} },
-  { 'nvim-mini/mini.comment', version = '*', opts = {} },
   { 'nvim-mini/mini.icons', version = '*', opts = {} },
   {
     'nvim-mini/mini.files',
