6548605c94d0a73a0f464905787da737b61235b6
shamnadxyz <shamnadxyz@proton.me>
2026-01-29 07:20:57 +0000

    feat(mini): change choose marked bind to <C-CR>

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,6 +6,44 @@ return {
       local pick = require("mini.pick")
 
       pick.setup({
+        -- Keys for performing actions. See `:h MiniPick-actions`.
+        mappings = {
+          caret_left        = '<Left>',
+          caret_right       = '<Right>',
+
+          choose            = '<CR>',
+          choose_in_split   = '<C-s>',
+          choose_in_tabpage = '<C-t>',
+          choose_in_vsplit  = '<C-v>',
+          choose_marked     = '<C-CR>',
+
+          delete_char       = '<BS>',
+          delete_char_right = '<Del>',
+          delete_left       = '<C-u>',
+          delete_word       = '<C-w>',
+
+          mark              = '<C-x>',
+          mark_all          = '<C-a>',
+
+          move_down         = '<C-n>',
+          move_start        = '<C-g>',
+          move_up           = '<C-p>',
+
+          paste             = '<C-r>',
+
+          refine            = '<C-Space>',
+          refine_marked     = '<M-Space>',
+
+          scroll_down       = '<C-f>',
+          scroll_left       = '<C-h>',
+          scroll_right      = '<C-l>',
+          scroll_up         = '<C-b>',
+
+          stop              = '<Esc>',
+
+          toggle_info       = '<S-Tab>',
+          toggle_preview    = '<Tab>',
+        },
         source = {
           show = pick.default_show
         },
