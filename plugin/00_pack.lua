b5a8bb9b1c4a10ddc05705f927acedf5cb576535
shamnadxyz <shamnadxyz@proton.me>
2026-04-18 13:51:27 +0000

    feat!: remove nvim treesitter plugin

--- plugin/00_pack.lua
+++ plugin/00_pack.lua
@@ -1,16 +1,3 @@
-vim.api.nvim_create_autocmd('PackChanged', {
-  group = vim.api.nvim_create_augroup('pack-treesitter-update', { clear = true }),
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == 'nvim-treesitter' and kind == 'update' then
-      if not ev.data.active then
-        vim.cmd.packadd 'nvim-treesitter'
-      end
-      vim.cmd 'TSUpdate'
-    end
-  end,
-})
-
 vim.pack.add {
   {
     src = 'https://github.com/saghen/blink.cmp',
@@ -46,10 +33,6 @@ vim.pack.add {
   {
     src = 'https://github.com/mfussenegger/nvim-lint',
   },
-  {
-    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
-    version = 'main',
-  }
 }
 
 vim.cmd ':packadd nvim.undotree'
