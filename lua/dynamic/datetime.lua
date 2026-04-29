9579d6ad6bc61efce99b76e288a4c308bf4f17d0
shamnadxyz <shamnadxyz@proton.me>
2026-04-29 16:43:23 +0000

    feat(snippet): add date snippet

--- lua/dynamic/datetime.lua
+++ lua/dynamic/datetime.lua
@@ -7,6 +7,10 @@ local function utc_iso_datetime()
   return os.date '!%Y-%m-%dT%H:%M:%SZ'
 end
 
+local function date()
+  return os.date '!%Y-%m-%d'
+end
+
 local function get_filename()
   return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ':t:r')
 end
@@ -19,6 +23,7 @@ end
 M.Items = {
   item('utcdatetime', utc_iso_datetime, ' UTC datetime in ISO format '),
   item('utctime', utc_time, 'UTC time (HH:SS)'),
+  item('date', date, 'Date (YYYY-MM-DD)'),
   item('filename', get_filename, 'Filename'),
 }
 
