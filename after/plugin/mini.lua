ed05f760d755affbf61d5a3d3cf16a2ca01d9f09
shamnadxyz <shamnadxyz@proton.me>
2026-05-11 19:46:26 +0000

    ui: use lighter colors for diff

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -8,6 +8,24 @@ end
 hl('StatusLine', { fg = '#d8d4cd', bg = nil })
 hl('TelescopeTitle', { fg = '#b3daf9', bg = '#11262d' })
 hl('Normal', { fg = '#d8d4cd', bg = nil })
+
+local neogit = {
+  diff_add = { bg = '#003415', fg = '#b8e1c1' },
+  diff_add_inline = { bg = '#3a7448', fg = '#d1ebd6' },
+  diff_add_highlight = { bg = '#295233', fg = '#addbb6' },
+  diff_delete = { bg = '#410d14', fg = '#fac5c7' },
+  diff_delete_inline = { bg = '#7b211f', fg = '#fde7e5' },
+  diff_delete_highlight = { bg = '#521615', fg = '#f9bdb6' },
+}
+
+hl('NeogitDiffAdd', neogit.diff_add)
+hl('NeogitDiffAddHighlight', neogit.diff_add_highlight)
+hl('NeogitDiffDelete', neogit.diff_delete)
+hl('NeogitDiffDeleteHighlight', neogit.diff_delete_highlight)
+hl('NeogitDiffDeleteInline', neogit.diff_delete_inline)
+hl('NeogitDiffAddInline', neogit.diff_add_inline)
+
+
 -- }}}
 
 -- vim:foldmethod=marker
