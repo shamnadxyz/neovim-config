3310903096d6a7bfbbed2cfdee242c37fefeebd7
shamnadxyz <shamnadxyz@proton.me>
2026-05-16 06:54:22 +0000

    feat: update lua_ls config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -41,17 +41,33 @@ local servers = {
   tailwindcss = {},
   ts_ls = {},
   lua_ls = {
+    on_init = function(client)
+      client.server_capabilities.documentFormattingProvider = false
+
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+          return
+        end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        runtime = {
+          version = 'LuaJIT',
+          path = { 'lua/?.lua', 'lua/?/init.lua' },
+        },
+        workspace = {
+          checkThirdParty = false,
+          library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+            '${3rd}/luv/library',
+            '${3rd}/busted/library',
+          }),
+        },
+      })
+    end,
     settings = {
       Lua = {
-        diagnostics = { globals = { 'vim' } },
-        runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
-        workspace = {
-          ignoreSubmodules = true,
-          library = { vim.env.VIMRUNTIME },
-          completion = {
-            callSnippet = 'Replace',
-          },
-        },
+        format = { enable = false },
       },
     },
   },
