74d0c1e7a08359a526ece31852a7b44cca9262e6
shamnadxyz <shamnadxyz@proton.me>
2026-06-12 22:22:32 +0000

    render-markdown: render heading
    Enabled heading and set heading background to Normal

--- after/plugin/render-markdown.lua
+++ after/plugin/render-markdown.lua
@@ -3,7 +3,16 @@ local render = require("render-markdown")
 render.setup({
   enabled = true,
   heading = {
-    enabled = false,
+    enabled = true,
+
+    backgrounds = {
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+      "Normal",
+    },
   },
   sign = { enabled = false },
 })
