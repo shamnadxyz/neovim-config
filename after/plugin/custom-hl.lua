a74d1400f5ff47c1eda6ecc7a75eaa946e7d3153
shamnadxyz <shamnadxyz@proton.me>
2026-06-27 07:31:54 +0000

    custom-hl: use autocmd to run the config

--- after/plugin/custom-hl.lua
+++ after/plugin/custom-hl.lua
@@ -1,3 +1,7 @@
-require('custom-hl').setup {
-  transparent = true,
-}
+vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
+  desc = 'Apply custom highlights after loading colorscheme',
+  group = vim.api.nvim_create_augroup('apply-hl', { clear = true }),
+  callback = function()
+    require('custom-hl').setup { transparent = false }
+  end,
+})
--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -57,11 +57,3 @@ vim.api.nvim_create_autocmd('PackChanged', {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-  desc = 'Apply custom highlights after loading colorscheme',
-  group = vim.api.nvim_create_augroup('apply-hl', { clear = true }),
-  callback = function()
-    require('custom-hl').setup { transparent = true }
-  end,
-})
