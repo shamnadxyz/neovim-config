581fc246c5de5e388f14510481606ef0620ed1d1
shamnadxyz <shamnadxyz@proton.me>
2026-06-28 02:19:03 +0000

    custom-hl: enable transparency

--- after/plugin/custom-hl.lua
+++ after/plugin/custom-hl.lua
@@ -1,7 +1,15 @@
-vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
+local custom_hl = require 'custom-hl'
+
+local config = {
+  transparent = true,
+}
+
+custom_hl.setup(config)
+
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
   desc = 'Apply custom highlights after loading colorscheme',
   group = vim.api.nvim_create_augroup('apply-hl', { clear = true }),
   callback = function()
-    require('custom-hl').setup { transparent = false }
+    custom_hl.setup(config)
   end,
 })
